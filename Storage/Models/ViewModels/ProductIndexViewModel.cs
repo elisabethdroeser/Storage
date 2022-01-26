4eda75a08e7583d0b32934ed221a12473d321dfa
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-01-26 18:49:19 +0100

    Update text/headers i table to swedish.

--- Storage/Models/ViewModels/ProductIndexViewModel.cs
+++ Storage/Models/ViewModels/ProductIndexViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Storage.Models.ViewModels
 {
     public class ProductIndexViewModel
     {
         public int Id { get; set; }  
+        
+        [Display(Name = "Produktnamn")]
         public string Name { get; set; }
+        [Display(Name = "Pris")]
         public int Price { get; set; }
+        [Display(Name = "Antal")]
         public int Count { get; set; }
+        [Display(Name = "Värde i kronor")]
         public int InventoryValue { get; set; }
     }
 }
