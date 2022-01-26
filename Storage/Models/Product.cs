0d8d46d6c2e1a1ba78b7f9a4ee66cb6aad2d90dc
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-01-26 14:58:33 +0100

    Work with database.

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,12 +1,16 @@
-﻿namespace Storage.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Storage.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
-        public int Price { get; set; }  
-        public DateTime OrderDate { get; set; } 
+        public string Name { get; set; }
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Range(1, 100)]
         public string Shelf { get; set; }
         public int Count { get; set; }
         public string Description { get; set; }
